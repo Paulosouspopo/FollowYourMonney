4d33a96557171c821a488f0258b92eeee6584ee7
Paulosouspopo <paul.arquier92700@gmail.com>
2026-07-12 18:28:03 +0200

    fix bug bdd

--- src/test/java/com/portfolio/tracker/TrackerApplicationTests.java
+++ src/test/java/com/portfolio/tracker/TrackerApplicationTests.java
@@ -2,18 +2,12 @@ package com.portfolio.tracker;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
-@TestPropertySource(properties = {
-    "spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration," +
-    "org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration," +
-    "org.springframework.boot.autoconfigure.flyway.FlywayAutoConfiguration"
-})
 class TrackerApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
